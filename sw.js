0a2cd41e7c3429309c1ca97eb2d974cb2d9a2e37
Beibei Jiang <beibei.jiang.research@outlook.com>
2026-07-16 13:49:40 +0200

    Refine nodule switching and VDT display

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'vdt-calculator-v3.3.0';
+const CACHE_NAME = 'vdt-calculator-v3.3.1';
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.json',
-  './vdt-core.js?v=3.3.0',
+  './vdt-core.js?v=3.3.1',
   './icons/icon.svg',
   './icons/icon-192.png',
   './icons/icon-512.png',
