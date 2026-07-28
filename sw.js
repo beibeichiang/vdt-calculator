6ff2280c9f7e8ad8cb44cfbb8fee01f3d96cb513
Beibei Jiang <beibei.jiang.research@outlook.com>
2026-07-28 14:44:22 +0200

    Remove total details from copy report

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'vdt-calculator-v3.3.6';
+const CACHE_NAME = 'vdt-calculator-v3.3.7';
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.json',
-  './vdt-core.js?v=3.3.6',
+  './vdt-core.js?v=3.3.7',
   './icons/icon.svg',
   './icons/icon-192.png',
   './icons/icon-512.png',
